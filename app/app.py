bda0d740116a91896bf17a6b3407002dcbbc6f35
Piachonkin-Alex <pechenkin.aa@phystech.edu>
2020-05-08 17:19:40 +0300

    non-worked

--- app/app.py
+++ app/app.py
@@ -1,18 +1,12 @@
 import sys
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-from flask import Flask, render_template
-from library import return_Articles
+from flask import Flask, render_template, request, redirect
+from library import Article, read_articles, add_article, change_article
 
 app = Flask(__name__)
 
-our_articles = return_Articles()
-
-
-@app.route('/')
-def start_page():
-    return render_template('index.html')
+our_articles = []
+read_articles(our_articles)
 
 
 @app.route('/about')
@@ -25,9 +19,36 @@ def go_articles():
     return render_template('articles.html', our_articles=our_articles)
 
 
-@app.route('/article/<string:id>/')
-def go_article(id):
-    return render_template('article_data.html', id=str(id))
+@app.route('/article/', methods=['GET', 'POST'])
+def go_article():
+    if request.method == 'POST':
+        change_article(our_articles, request.args['text'], request.args['id'], request.args['title'],
+                       request.args['authors'])
+        return render_template('article_data.html', id=request.args['id'], text=request.args['text'],
+                               author=request.args['author'], title=request.args['title'])
+    return render_template('article_data.html', id=request.args['v1'], text=request.args['v2'],
+                           author=request.args['v3'], title=request.args['v4'])
+
+
+@app.route('/new_article', methods=['GET', 'POST'])
+def create_new_article():
+    return render_template('article_page.html', id=request.args['id'], text=request.args['text'],
+                           author=request.args['author'], title=request.args['title'])
+
+
+@app.route('/edit_article', methods=['GET', 'POST'])
+def edit_article():
+    if request.method == 'POST':
+        return render_template('article_edit_page', id=request.args['id'], text=request.args['text'],
+                               author=request.args['author'], title=request.args['title'])
+
+
+@app.route('/', methods=['POST'])
+def start_menu():
+    if request.method == 'POST':
+        add_article(our_articles, request.form['article'], len(our_articles), request.form['ArticleName'],
+                    request.form['AuthorName'])
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
--- app/library.py
+++ app/library.py
@@ -1,22 +1,48 @@
 # /usr/bin/env python
 # -*- coding: utf-8 -*-
+from typing import Any
+import json
+
+
 class Article():
     def __init__(self):
         self.id = None
         self.title = ""
-        self.generate_data = ""
         self.text = ""
         self.authors = ""
 
 
-def return_Articles():
-    articles = []
-    for i in range(1, 4):
+articles = []
+
+
+def read_articles(articles):
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+    for i in range(len(data)):
         article = Article()
-        article.id = i
-        article.title = "Статья " + str(i)
-        article.text = 'Article'
-        article.generate_data = "28.04.2020"
-        article.authors = "Alex"
+        article.id = data[i]['id']
+        article.title = data[i]['title']
+        article.text = data[i]['text']
+        article.authors = data[i]['authors']
         articles.append(article)
-    return articles
+
+
+def add_article(articles, text: str, id: int, title: str, authors: str) -> None:
+    article = Article()
+    article.id = id
+    article.title = title
+    article.text = text
+    article.authors = authors
+    articles.append(article)
+    data = []
+    for i in range(len(articles)):
+        data.append({'id': articles[i].id, 'title': articles[i].title, 'authors': articles[i].authors, 'text':
+            articles[i].text})
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
+
+
+def change_article(articles, new_text: str, id: int, new_title: str, new_authors: str) -> None:
+    articles[id].text = new_text
+    articles[id].authors = new_authors
+    articles[id].title = new_title
