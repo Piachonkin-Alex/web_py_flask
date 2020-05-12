7e54436be13a411e48337708f1ca03d38b1399fd
Piachonkin-Alex <pechenkin.aa@phystech.edu>
2020-05-12 13:57:25 +0300

    fixed

--- app/library.py
+++ app/library.py
@@ -23,29 +23,33 @@ def read_articles(file_: str, articles):  ## считывание с файла
         data = json.load(f)
     for i in range(len(data)):
         article = Article()
-        article.fill_fields(data[i]['id'], data[i]['title'], data[i]['text'], data[i]['authors'])
+        args = [data[i]['id'], data[i]['title']]
+        args += [data[i]['text'], data[i]['authors']]
+        article.fill_fields(*args)
         articles.append(article)
 
 
-def add_article(file, articles: List, text: str, id: int, title: str, authors: str) -> None:
+def add_article(file, articles, text, id, title, authors) -> None:
     article = Article()  ## добавление статьи в список
     article.fill_fields(id, title, text, authors)
     articles.append(article)
     data = []
     for i in range(len(articles)):
-        data.append({'id': articles[i].id, 'title': articles[i].title, 'authors': articles[i].authors, 'text':
-            articles[i].text})  ## запись в файл
+        dict_ = {'id': articles[i].id, 'title': articles[i].title}
+        dict_.update({'authors': articles[i].authors, 'text': articles[i].text})
+        data.append(dict_)  ## запись в файл
         with open(file, 'w') as f:
             json.dump(data, f)
 
 
-def change_article(file_, articles: List, new_text: str, id: int, new_title: str, new_authors: str) -> None:
+def change_article(file_, articles, new_text, id: int, new_title, new_authors) -> None:
     articles[int(id)].text = new_text  ## изменение статьи
     articles[id].authors = new_authors
     articles[id].title = new_title
     data = []
     for i in range(len(articles)):
-        data.append({'id': articles[i].id, 'title': articles[i].title, 'authors': articles[i].authors, 'text':
-            articles[i].text})  ## запись в файл
+        dict_ = {'id': articles[i].id, 'title': articles[i].title}
+        dict_.update({'authors': articles[i].authors, 'text': articles[i].text})
+        data.append(dict_)  ## запись в файл
         with open(file_, 'w') as f:
             json.dump(data, f)
